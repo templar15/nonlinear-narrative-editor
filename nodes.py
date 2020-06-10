2c35779ce89281486642eb3b41db558dbf098c10
templar15 <martysun@live.cn>
2020-06-10 16:41:16 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,29 +1,40 @@
 #file contains the definitions of nodes or script segments 
+
+'''
+	Connections are the decisions made while nodes are the actual script content
+'''
 file_path = "scripts/"
 class connection:#edge connecting two nodes
 	self.parent = None
 	self.child = None
-	self.duration = 0
+	decision = None	#the decision made (also the file name)
 
-	def __init__(nodeA, nodeB,duration):
+	def __init__(self,nodeA, nodeB,decision):#nodeA, nodeB should be pointers to objects while decision should be a string
 		self.parent = nodeA
 		self.child = nodeB
-		self.duration = duration
-
-	def modifyDuration(newDur):
-		self.duration = newDur
-
+		self.decision = decision
+	def rename(decision):
+		self.decision = decision
+'''
+	adj list not used as there could be multiple connections between two nodes (insignificant decisions that effect future outcomes and not the current one)
+'''
 class Node:
 	#dictionary might be better here
 	connections = [] #connections to other nodes (for script connections as defined above) these are out going edges
 	inConnection = [] #connections to nodes that lead into said edge for later deletion/ termination of current node
 	name = None
-	text = None
-	def __init__(self, name, load=False):#init
+	text = None #actual script
+	self.duration
+
+	def __init__(self, name, load=False, duration=None):#init
 		self.name = name 
 		if load != False:
 			read()
-		
+		self.duration = duration
+	
+	def modifyDuration(newDur):
+		self.duration = newDur	
+
 	def write():
 		file = open(file_path+name,"r")
 		self.text = file.read()
@@ -37,7 +48,10 @@ class Node:
 	def add_connection(toNode,duration=None):
 		self.connections.append(connection(self,toNode,duration))
 		toNode.inConnection.append(self)
-	def delete_connection():
 
+	def delete_connection():
+		#TODO:
+		
 	def delete():#need to delete all edges connecting to self as well!
+		#TODO:
 		
